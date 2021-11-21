103ec11146dc0c167f01f1cffb4ac8bf962e9bf6
kbk1303 <kbk1303@gmail.com>
2021-11-21 10:47:10 +0100

    .net 5.0

--- TestAppAngular/Startup.cs
+++ TestAppAngular/Startup.cs
@@ -24,13 +24,15 @@ namespace TestAppAngular
             /* entity framework */
             services.AddDbContextPool<Informationcontext>(options => options.UseSqlServer(Configuration.GetConnectionString("InformationDB")));
             
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddMvc(options => options.EnableEndpointRouting = false);
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
